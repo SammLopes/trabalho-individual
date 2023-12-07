71c34b27206537d222027c300f1eaed82e067506
Samuel Lopes <sam.v.p.lopes@gmail.com>
2023-12-07 17:48:58 -0300

    Update calendário

--- src/components/calendario/calendario.js
+++ src/components/calendario/calendario.js
@@ -1,132 +1,118 @@
-import React, { useState, useEffect } from "react"; 
+import React, { useState, useEffect } from "react";
 import Table from 'react-bootstrap/Table';
 
-
 const Calendario = () => {
-    const diasDaSemana = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado'];
-    const [desafios, setDesafios] = useState([]);
-    useEffect(() => {
-      const storageDesafios =  JSON.parse(localStorage.getItem('desafios')) || [];
-      setDesafios(storageDesafios);
-      return() => {}
-    }, []);
+  const diasDaSemana = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado'];
+  const [desafios, setDesafios] = useState([]);
+  const [diaSelected, setDia] = useState(null);
 
-    
+  useEffect(() => {
+    const storageDesafios = JSON.parse(localStorage.getItem('desafios')) || [];
+    setDesafios(storageDesafios);
+  }, []);
 
-    const [dataAtual, setDataAtual] = useState(new Date());
-    const [diaSelected, setDia] = useState(null);
-    
-    const handleDayClick = (dia) => {
-      console.log("2");
-      setDia(dia);
-    };
-    
-      const events = {};
-      const getDiasMes = (data) =>{
-      
-        const ano = data.getFullYear();
-        const mes = data.getMonth();
-        const primeiroDia = new Date(ano, mes, 1);
-        const ultimoDia = new Date(ano, mes + 1, 0);
-        const comecoDia = primeiroDia.getDay();
-        const fimDia = ultimoDia.getDate();
-    
-        const dia = [];
-        let semanaAtual = [];
-    
-        for (let i =0; i< comecoDia; i++) {
-          semanaAtual.push("");
-        }
-    
-        for (let day = 1; day <= fimDia; day++) {
-          const dataAtual = new Date(ano, mes, day);
-          semanaAtual.push(
-            `${ano}-${(mes + 1).toString().padStart(2, "0")}-${day
-              .toString()
-              .padStart(2, "0")}`
-          );
-          if (semanaAtual.length === 7) {
-            dia.push([...semanaAtual]);
-            semanaAtual = [];
-          }
-        }
-    
-        if (semanaAtual.length > 0) {
-          while (semanaAtual.length < 7) {
-            semanaAtual.push("");
-          }
-          dia.push([...semanaAtual]);
-        }
-    
-        return dia;
+  const getDiasMes = (ano, mes) => {
+    const primeiroDia = new Date(ano, mes, 1);
+    const ultimoDia = new Date(ano, mes + 1, 0);
+    const comecoDia = primeiroDia.getDay();
+    const fimDia = ultimoDia.getDate();
+
+    const dia = [];
+    let semanaAtual = [];
+
+    for (let i = 0; i < comecoDia; i++) {
+      semanaAtual.push("");
     }
 
-   
-    console.log(desafios);
-    return(
-        <>
-          <h1>Calendario de Novembro</h1>
-          {desafios.length > 0 ?(
-          <Table striped bordered hover variant="dark" className="custom-calendar">
-            <thead>
-              <tr>
+    for (let day = 1; day <= fimDia; day++) {
+      const dataAtual = new Date(ano, mes, day);
+      semanaAtual.push(
+        `${ano}-${(mes + 1).toString().padStart(2, "0")}-${day
+          .toString()
+          .padStart(2, "0")}`
+      );
+      if (semanaAtual.length === 7) {
+        dia.push([...semanaAtual]);
+        semanaAtual = [];
+      }
+    }
+
+    if (semanaAtual.length > 0) {
+      while (semanaAtual.length < 7) {
+        semanaAtual.push("");
+      }
+      dia.push([...semanaAtual]);
+    }
+
+    return dia;
+  }
+
+  const anoAtual = new Date().getFullYear();
+  const mesAtual = 10; // Coloque o número do mês desejado (0 a 11, sendo 0 janeiro e 11 dezembro)
+
+  const [dataAtual, setDataAtual] = useState(new Date(anoAtual, mesAtual));
+
+  const handleDayClick = (dia) => {
+    setDia(dia);
+  };
+
+  return (
+    <>
+      <h1>Calendario de Novembro</h1>
+      {desafios.length > 0 ? (
+        <Table striped bordered hover variant="dark" className="custom-calendar">
+          <thead>
+            <tr>
               {diasDaSemana.map((day) => (
-              <th key={day}>{day}</th>
-            ))}
-             </tr>
-            </thead>
-            <tbody>
-              {getDiasMes(dataAtual).map((semana, index) => (
-                
-                <tr key={index}>
+                <th key={day}>{day}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {getDiasMes(dataAtual.getFullYear(), dataAtual.getMonth()).map((semana, index) => (
+              <tr key={index}>
                 {semana.map((dia, colIndex) => (
                   <td
-                  key={colIndex}
-                  onClick={() =>
-                    handleDayClick(
-                      `${dataAtual.getFullYear()}-${(dataAtual.getMonth() + 1).toString().padStart(2, "0")}-${dia.toString().padStart(2, "0")}`
-                      
+                    key={colIndex}
+                    onClick={() =>
+                      handleDayClick(
+                        `${dataAtual.getFullYear()}-${(dataAtual.getMonth() + 1).toString().padStart(2, "0")}-${dia.toString().padStart(2, "0")}`
                       )
                     }
-                    >
-                    <div >{dia}</div>
-                    <div >
-                      {dia !== '' && // Verifica se o dia não está vazio
-                      events[diasDaSemana[colIndex]] &&
-                      events[diasDaSemana[colIndex]].map((desafios, index) => {
-                        const dataInicio = new Date(desafios.dataInicio);
-                        const dataFim = new Date(desafios.dataFim);
-                        console.log(desafios.dataInicio);
-                        console.log(desafios.dataFim);
-                        // Verifica se o dia está dentro do intervalo de datas
-                        if (dataAtual >= dataInicio && dataAtual <= dataFim) {
-                          //{console.log(diasDaSemana[colIndex])}
-                          //console.log("Passou");
-                          return (
-                            <div key={index}>
-                              {desafios.desafio} - {desafios.horarioStart}- {desafios.horarioEnd} -
-                              {desafios.professor} - {desafios.sala}
-                            </div>
-                          );
-                        }
-                    
-                        return null; // Retorna null se o dia não estiver no intervalo de datas
-                      })}
+                  >
+                    <div>{dia}</div>
+                    <div>
+                      {dia !== '' &&
+                        desafios.map((desafio, index) => {
+                          const dataInicio = new Date(desafio.dataInicio);
+                          const dataFim = new Date(desafio.dataFim);
+                          const semanaDesafio = desafio.semana
+                          const d = new Date(dia);
+                          const isExitsDia = d >= dataInicio && d <= dataFim;
+                          const isExitsSemana = diasDaSemana[colIndex] == semanaDesafio;
+                          if (isExitsDia && isExitsSemana) {
+                            return (
+                              <div key={index}>
+                                {desafio.desafio} - {desafio.horarioStart} - {desafio.horarioEnd} -
+                                {desafio.professor} - {desafio.sala}
+                              </div>
+                            );
+                          }
+                          return null;
+                        })
+                      }
                     </div>
                   </td>
                 ))}
               </tr>
             ))}
-              </tbody>
-          </Table>
-         
-       ) : (
-            <p className='info'>Nenhum dado disponível!!</p>
-          )}
-    
-           
-        </>
-    )
+          </tbody>
+        </Table>
+      ) : (
+        <p className='info'>Nenhum dado disponível!!</p>
+      )}
+    </>
+  )
 }
 
 export default Calendario;
--- src/pages/cadHorarios/calendario.js
+++ src/pages/cadHorarios/calendario.js
@@ -1,13 +1,16 @@
 import Navigation from "../../components/navbar";
 import Footer from "../../components/footer";
 import Calendario from "../../components/calendario/calendario.js";
+import Cal from "../../components/calendario/_calendario.js";
+
 import '../../assets/css/calendario.css';
 const pageCalendario = () => {
     return(
         <>
             <Navigation/>
             <div className="form-comun">
-                <Calendario/>
+            <Calendario/>
+                {/* <Cal/> */}
             </div>
             <Footer/>
         </>
