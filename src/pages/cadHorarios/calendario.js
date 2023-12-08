223260bd7cda5559216da88005913c2160f595e1
Samuel Lopes <samuelventurini2017@outlook.com>
2023-12-07 22:48:53 -0300

    Delete arquivo _calendario.js

--- src/pages/cadHorarios/calendario.js
+++ src/pages/cadHorarios/calendario.js
@@ -1,16 +1,14 @@
 import Navigation from "../../components/navbar";
 import Footer from "../../components/footer";
 import Calendario from "../../components/calendario/calendario.js";
-import Cal from "../../components/calendario/_calendario.js";
-
 import '../../assets/css/calendario.css';
+
 const pageCalendario = () => {
     return(
         <>
             <Navigation/>
             <div className="form-comun">
-            <Calendario/>
-                {/* <Cal/> */}
+                <Calendario/>
             </div>
             <Footer/>
         </>
