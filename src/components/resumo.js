fcc64ecf7eefea3546a6082fbb01e6d6566d2ae6
Samuel Lopes <sam.v.p.lopes@gmail.com>
2023-12-11 15:19:05 -0300

    Update cards restantes

--- src/components/resumo.js
+++ src/components/resumo.js
@@ -9,6 +9,8 @@ const Resumo = () => {
     const [cursos, setCursos] = useState([]);
     const [periodos, setPeriodos] = useState([]);
     const [professores, setProfesores] = useState([]);
+    const [salas, setSalas] = useState([]);
+    const [desafios, setDesafios] = useState([]);
     useEffect(() => {
         const getCursos = JSON.parse(localStorage.getItem('cursos')) || [];
         setCursos(getCursos);
@@ -16,6 +18,10 @@ const Resumo = () => {
         setPeriodos(getPeriodos);
         const getProfessores = JSON.parse(localStorage.getItem('professores'))|| [];
         setProfesores(getProfessores);
+        const getSalas = JSON.parse(localStorage.getItem('salas'));
+        setSalas(getSalas);
+        const getDesafios = JSON.parse(localStorage.getItem('desafios'));
+        setDesafios(getDesafios)
       }, []);
     return(
         <>
@@ -32,6 +38,16 @@ const Resumo = () => {
                 <div className='card-professor'>
                     <CardProfessor professores={professores}/>
                 </div>
+                {
+                /*
+                <div className='card-sala'>
+                    <CardProfessor salas={salas}/>
+                </div>
+                <div className='card-desafio'>
+                    <CardProfessor desafios={desafios}/>
+                </div>
+                */
+                }
             </div>
         </>
     );
---
+++ src/components/resumos/cardDesafio.js
@@ -0,0 +1,25 @@
+import Card from 'react-bootstrap/Card';
+import { Link } from 'react-router-dom'; 
+import Button from 'react-bootstrap/Button';
+
+const ResumoDesafios = ({ desafios }) => {
+
+    const size = desafios.length;
+    return (
+      <div >
+        <Card style={{ width: '18rem', color:'white'}}  bg="dark">
+            <Card.Img variant="top"  src='/icons/favicons/android-chrome-192x192.png' className="img-fluid" style={{ maxWidth: '100%', maxHeight: '250px', marginTop:'10px' }} />
+            <Card.Body>
+                <Card.Title>Desafios</Card.Title>
+                <Card.Text>Quantidade de desafios: {size}</Card.Text>
+            </Card.Body>
+            <Card.Body>
+                <Button size="sm" as={Link} to="/desafio">Cadastros de desafio</Button>
+            </Card.Body>
+        </Card>
+
+      </div>
+    );
+   }
+
+   export default ResumoDesafios;
